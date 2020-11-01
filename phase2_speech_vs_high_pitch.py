a22009bdfb1c6e5b725fda9321a7922f6d728a7a
Vinay Sharma <45567648+DR-NEGATIVE@users.noreply.github.com>
2020-11-01 19:57:30 +0530

    Update phase2_speech_vs_high_pitch.py

--- phase2_speech_vs_high_pitch.py
+++ phase2_speech_vs_high_pitch.py
@@ -5,5 +5,5 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 x_train = genfromtxt('speech.csv',delimiter=',')
 y_train = genfromtxt('id_of_it.csv',delimiter=',')
-second_para = svm.SVC(kernel='rbf')
+second_para = svm.SVC(C=20.0,gamma=0.00001)
 second_para.fit(x_train, y_train)
